7e99f13ef9426cf530df034880278b87facd828d
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2025-04-20 17:27:05 +0200

    refactor(shared-ui-core): simplify animation configuration

--- packages/shared/ui/core/src/theme/animations.ts
+++ packages/shared/ui/core/src/theme/animations.ts
@@ -1,8 +1,7 @@
 import { createAnimations } from '@tamagui/animations-css'
-import type { AnimationConfigType } from '@tamagui/web'
 
 // You might need to import the correct type from the package
-export const animations: AnimationConfigType = createAnimations({
+export const animations = createAnimations({
 	bouncy: '800ms cubic-bezier(0.34, 1.56, 0.64, 1) both',
 	fast: 'ease-in 150ms',
 	medium: 'ease-in 300ms',
