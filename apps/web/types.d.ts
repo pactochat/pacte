7d99b9ee98f5f78848058cccf50e68a045ff84c0
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2025-04-28 11:49:17 +0200

    refactor(apps-web): remove deprecated web

--- apps/web/types.d.ts
+++
@@ -1,9 +0,0 @@
-import type { tamaguiConfig } from '@pacto-chat/shared-ui-core/theme'
-
-export type CustomTamaguiConfigType = typeof tamaguiConfig
-
-declare module '@pacto-chat/shared-ui-core/theme' {
-	interface ICustomTamaguiConfig extends CustomTamaguiConfigType {}
-}
-
-export default tamaguiConfig
